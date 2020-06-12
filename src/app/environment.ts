ab9f6f7cf0b60260854b6c6e5a4bd55f389de8e9
Yojan Pardo <yojan.pardo@gmail.com>
2020-06-12 18:20:05 -0500

    change pokemon api context

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 
 export class Environment{
-    // myPokemonApiContext = 'http://3.128.62.213/api/v1/';
-    myPokemonApiContext = 'http://localhost:8080/api/v1/';
+    myPokemonApiContext = 'http://3.128.62.213/api/v1/';
+    // myPokemonApiContext = 'http://localhost:8080/api/v1/';
     pokemonContext = 'pokemon/';
     pokemonSpeciesContext = 'pokemon-species/';
     evolutionChainContext = 'evolution-chain/';
